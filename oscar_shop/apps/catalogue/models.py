edc735ad3fdf242b4421da06a0ac3406ab5637f3
drashtij17 <103404734+drashtij17@users.noreply.github.com>
2022-07-15 18:12:22 +0530

    Old code

--- oscar_shop/apps/catalogue/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from oscar.apps.catalogue.abstract_models import AbstractProduct
-
-class Product(AbstractProduct):
-    place = models.CharField(max_length=40,blank=True,null=True)
-
-
-
-from oscar.apps.catalogue.models import *  # noqa isort:skip
--- oscar_shop/apps/customer/views.py
+++
@@ -1,4 +0,0 @@
-# from oscar.apps.customer.views import ProfileView
-
-# class Proflie(ProfileView):
-#     template_name = 'oscar/customer/profile/profile.html'
