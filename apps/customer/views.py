d70b1237c31e808847eec744577646193bc479fd
drashtij17 <103404734+drashtij17@users.noreply.github.com>
2022-07-18 18:32:42 +0530

    commit-2

--- apps/customer/views.py
+++ apps/customer/views.py
@@ -2,3 +2,11 @@
 
 # class Proflie(ProfileView):
 #     template_name = 'oscar/customer/profile/profile.html'
+
+from django.views import generic
+
+class ContView(generic.TemplateView):
+    
+    template_name = 'oscar/customer/contact.html'
+
+
